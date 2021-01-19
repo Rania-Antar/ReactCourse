deb6e8b55484ed1d28af58581f0618b50f1a8ec1
Rania ANTAR <rania.antar@esprit.tn>
2021-01-19 10:33:37 +0100

    coursera react course

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'font-awesome/css/font-awesome.css';
+// import 'bootstrap-social/bootstrap-social.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render (
+    <App/>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/shared/baseUrl.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'http://localhost:3001/';
+
+//npx json-server --watch db.json --port 3001 -d 2000
